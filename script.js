ca9f0dca0177d107e47a34181bda7e5991160387
Siljeo7 <siljekv07@icloud.com>
2025-06-02 09:53:29 +0200

    produkt side

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+const images = document.querySelectorAll('.bilder img');
+    let current = 0;
 
+    function showImage(index) {
+      images.forEach((img, i) => {
+        img.classList.toggle('active', i === index);
+      });
+    }
+
+    function nextImage() {
+      current = (current + 1) % images.length;
+      showImage(current);
+    }
+
+    function prevImage() {
+      current = (current - 1 + images.length) % images.length;
+      showImage(current);
+    }
